7e18ed51400ac1361a90bbf5b4f60fe8a01fd0c8
ryuki nishikawa <xuexiaoshiyong575@gmail.com>
2025-05-20 10:46:20 +0900

    okinawa

--- sample.java
+++ sample.java
@@ -4,7 +4,7 @@ public class sample {
         String word_i = "いつもの朝とは様子が違う";
         String word_u = "嬉しい知らせが飛び込んできた";
         String word_e = "駅で急に待ち合わせ。慌てて準備して";
-        String word_o = "温泉旅行に出発だ！";
+        String word_o = "okinawa旅行に出発だ！";
 
         System.out.println("あ：" + word_a);
         System.out.println("い：" + word_i);
