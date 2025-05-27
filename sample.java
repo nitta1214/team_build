f9c71bd94733307bbdb8c0ce5d8c97121b353b76
ryuki nishikawa <xuexiaoshiyong575@gmail.com>
2025-05-27 10:11:16 +0900

    片山先生に変更

--- sample.java
+++ sample.java
@@ -1,15 +1,17 @@
 public class sample {
     public static void main(String[] args) {
-        String word_a = "朝から驚きの連続だ";
-        String word_i = "いつもの朝とは様子が違う";
-        String word_u = "嬉しい知らせが飛び込んできた";
-        String word_e = "駅で急に待ち合わせ。慌てて準備して";
-        String word_o = "okinawa旅行に出発だ！";
+        String word_ka = "朝から驚きの連続だ";
+        String word_ta = "いつもの朝とは様子が違う";
+        String word_ya = "嬉しい知らせが飛び込んできた";
+        String word_ma = "駅で急に待ち合わせ。慌てて準備して";
+        String word_sen = "okinawa旅行に出発だ！";
+        String word_sei = "";
 
-        System.out.println("あ：" + word_a);
-        System.out.println("い：" + word_i);
-        System.out.println("う：" + word_u);
-        System.out.println("え：" + word_e);
-        System.out.println("お：" + word_o);
+        System.out.println("か：" + word_ka);
+        System.out.println("た：" + word_ta);
+        System.out.println("や：" + word_ya);
+        System.out.println("ま：" + word_ma);
+        System.out.println("先：" + word_sen);
+        System.out.println("生：" + word_sei);
     }
 }
