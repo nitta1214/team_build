c88544cfcfb651342c396f3cd13a80e531a79926
ryuki nishikawa <xuexiaoshiyong575@gmail.com>
2025-05-27 10:28:44 +0900

    ま：を変更

--- sample.java
+++ sample.java
@@ -1,24 +1,9 @@
 public class sample {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        String word_ka = "朝から驚きの連続だ";
-        String word_ta = "いつもの朝とは様子が違う";
-        String word_ya = "嬉しい知らせが飛び込んできた";
-        String word_ma = "駅で急に待ち合わせ。慌てて準備して";
-        String word_sen = "okinawa旅行に出発だ！";
-        String word_sei = "";
-
-        System.out.println("か：" + word_ka);
-        System.out.println("た：" + word_ta);
-        System.out.println("や：" + word_ya);
-        System.out.println("ま：" + word_ma);
-        System.out.println("先：" + word_sen);
-        System.out.println("生：" + word_sei);
-=======
         String word_a = "朝から驚きの連続だ";
         String word_i = "いつもの朝とは様子が違う";
         String word_u = "嬉しい知らせが飛び込んできた";
-        String word_e = "駅で急に待ち合わせ。慌てて準備して";
+        String word_e = "マクドナルドに行きたいな";
         String word_o = "okinawa旅行に出発だ";
         String word_k = "";
 
@@ -28,6 +13,5 @@ public class sample {
         System.out.println("ま：" + word_e);
         System.out.println("先：" + word_o);
         System.out.println("生：" + word_k);
->>>>>>> 31b101830728e7eb2387f1f363b18f438ecf4da4
     }
 }
