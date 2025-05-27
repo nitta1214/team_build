17083232fc799e999017c797ea04787945f15501
shimoyama souta <shimoyama.git@gmail.com>
2025-05-27 09:56:12 +0900

    オ行を「温泉旅行に出発しよう」に変更

--- sample.js
+++ sample.js
@@ -2,7 +2,8 @@ const word_a = "朝から驚きの連続だ";
 const word_i = "いつもの朝とは様子が違う";
 const word_u = "嬉しい知らせが飛び込んできた";
 const word_e = "駅で急に待ち合わせ。慌てて準備して";
-const word_o = "温泉旅行に出発だ！";
+const word_o = "温泉旅行に出発しよう！";
+
 
 const outputDiv = document.getElementById("output");
 outputDiv.innerHTML = `
