420dc290edda10e1fb9755adedaf0cf36793972e
chinen jyouichirou <jyouichirou113377plusultrap@gmail.com>
2025-05-27 10:16:30 +0900

    片山先生に変更

--- sample.java
+++ sample.java
@@ -4,12 +4,14 @@ public class sample {
         String word_i = "いつもの朝とは様子が違う";
         String word_u = "嬉しい知らせが飛び込んできた";
         String word_e = "駅で急に待ち合わせ。慌てて準備して";
-        String word_o = "okinawa旅行に出発だ！";
-      
-        System.out.println("あ：" + word_a);
-        System.out.println("い：" + word_i);
-        System.out.println("う：" + word_u);
-        System.out.println("え：" + word_e);
-        System.out.println("お：" + word_o);
+        String word_o = "okinawa旅行に出発だ";
+        String word_k = "";
+
+        System.out.println("か：" + word_a);
+        System.out.println("た：" + word_i);
+        System.out.println("や：" + word_u);
+        System.out.println("ま：" + word_e);
+        System.out.println("先：" + word_o);
+        System.out.println("生：" + word_k);
     }
 }
