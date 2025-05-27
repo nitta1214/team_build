fc36a54cc33c88e55e43f7bd62e68d3025191fc3
chinen jyouichirou <jyouichirou113377plusultrap@gmail.com>
2025-05-27 10:44:16 +0900

    全体的に変更

--- sample.java
+++ sample.java
@@ -1,17 +1,17 @@
 public class sample {
     public static void main(String[] args) {
-        String word_a = "かたつむりがいた";
-        String word_i = "いつもの朝とは様子が違う";
-        String word_u = "やる気を持って";
-        String word_e = "マクドナルドに行きたいな";
-        String word_o = "先日の出来事が体を破壊";
-        String word_k = "";
+        String word_ka = "かたつむりがいた";
+        String word_ta = "いつもの朝とは様子が違う";
+        String word_ya = "やる気を持って";
+        String word_ma = "マクドナルドに行きたいな";
+        String word_sen = "先日の出来事が体を破壊";
+        String word_sei = "生きることは素晴らしい";
 
-        System.out.println("か：" + word_a);
-        System.out.println("た：" + word_i);
-        System.out.println("や：" + word_u);
-        System.out.println("ま：" + word_e);
-        System.out.println("先：" + word_o);
-        System.out.println("生：" + word_k);
+        System.out.println("か：" + word_ka);
+        System.out.println("た：" + word_ta);
+        System.out.println("や：" + word_ya);
+        System.out.println("ま：" + word_ma);
+        System.out.println("先：" + word_sen);
+        System.out.println("生：" + word_sei);
     }
 }
