6172bd0273fad6fc6bb8cbbf017086d2d1210509
chinen jyouichirou <jyouichirou113377plusultrap@gmail.com>
2025-06-03 09:50:35 +0900

    jsに野木先生を追加

--- sample.js
+++ sample.js
@@ -1,15 +1,16 @@
-const word_a = "朝から驚きの連続だ!";
-const word_i = "いつもの朝とは様子が違う";
-const word_u = "嬉しい知らせが飛び込んできた！";
-const word_e = "駅で急に待ち合わせ。慌てて準備して";
-const word_o = "温泉旅行に出発しよう！";
-
+const word_no = "朝から驚きの連続だ!";
+const word_gi = "いつもの朝とは様子が違う";
+const word_ma = "嬉しい知らせが飛び込んできた！";
+const word_sa = "駅で急に待ち合わせ。慌てて準備して";
+const word_hu = "温泉旅行に出発しよう！";
+const word_mi = "あの人と会うのが楽しみだ";
 
 const outputDiv = document.getElementById("output");
 outputDiv.innerHTML = `
-    <p>あ：${word_a}</p>
-    <p>い：${word_i}</p>
-    <p>う：${word_u}</p>
-    <p>え：${word_e}</p>
-    <p>お：${word_o}</p>
+    <p>の：${word_no}</p>
+    <p>ぎ：${word_gi}</p>
+    <p>ま：${word_ma}</p>
+    <p>さ：${word_sa}</p>
+    <p>ふ：${word_hu}</p>
+    <p>み：${word_mi}</p>
 `;
