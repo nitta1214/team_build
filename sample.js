06cbd0e10f8d66b19eb13420ec38e8f0865a9410
haru503 <matsumoto.haruka53@gmail.com>
2025-06-03 09:53:08 +0900

    ふ行を変更

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@ const word_no = "朝から驚きの連続だ!";
 const word_gi = "いつもの朝とは様子が違う";
 const word_ma = "嬉しい知らせが飛び込んできた！";
 const word_sa = "駅で急に待ち合わせ。慌てて準備して";
-const word_hu = "温泉旅行に出発しよう！";
+const word_hu = "服びしょびしょでマジ最悪";
 const word_mi = "あの人と会うのが楽しみだ";
 
 const outputDiv = document.getElementById("output");
