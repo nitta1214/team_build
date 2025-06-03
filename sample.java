5cefa5f7327022d947072f01d56ade27e6fe00b6
東南　和磨 <tk18650421@gmail.com>
2025-06-03 09:20:26 +0900

    た行変更

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ public class sample {
     public static void main(String[] args) {
 
         String word_a = "かたつむりがいた";
-        String word_i = "いつもの朝とは様子が違う";
+        String word_i = "鷹に警戒しながら";
         String word_u = "やる気を持って";
         String word_e = "マクドナルドに行きたいな";
         String word_o = "先日の出来事が体を破壊";
