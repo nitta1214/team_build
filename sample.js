7ef8644d04665731092c4e7aad8606e4645e0935
shimoyama souta <shimoyama.git@gmail.com>
2025-06-03 09:57:35 +0900

    さを変更

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 const word_no = "朝から驚きの連続だ!";
 const word_gi = "いつもの朝とは様子が違う";
 const word_ma = "嬉しい知らせが飛び込んできた！";
-const word_sa = "駅で急に待ち合わせ。慌てて準備して";
+const word_sa = "サービス残業";
 const word_hu = "温泉旅行に出発しよう！";
 const word_mi = "あの人と会うのが楽しみだ";
 
